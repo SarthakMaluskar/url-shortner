8700a532428943251928930cd4fd2b1cb46a0d58
SarthakMaluskar <sarthak.malu1@gmail.com>
2026-07-19 22:50:08 +0530

    added nanoId instead of custom short code generation

--- server/utils/shortCodeGen.js
+++ server/utils/shortCodeGen.js
@@ -2,19 +2,18 @@
 //my idea is to create an array with all chars like A-Z and a-z and 0-9 then use inbuilt math.random func to pick a num from 0 - 61 so it chooses a random char and then i ll append to a string this process 6 times
 //but this seems slow for production grade project, thinking of other way
 
+const { nanoid, customAlphabet } = require('nanoid');
+
 
 function ShortCodeGen(){
-    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    let shortCode = "";
 
-    //coz we are createing a code of 6 digits
-    for(let i =0;i < 6;i++){
-        const randDigit = Math.floor(Math.random() * chars.length);
-        shortCode += chars.charAt(randDigit);
-    }
-    
+    const generateMyID = customAlphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", 6);
+    const shortCode = generateMyID();
+    console.log(shortCode);
     return shortCode;
 
 }
+
+
 
 module.exports = ShortCodeGen;
