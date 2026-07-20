bb6de868763e34f28263f50760e5a3f7e7a34683
SarthakMaluskar <sarthak.malu1@gmail.com>
2026-07-20 22:41:34 +0530

    created custom error handeling classes

---
+++ server/errors/AppError.js
@@ -0,0 +1,13 @@
+class AppError extends Error{
+    constructor(message,status){
+        super(message);
+
+        this.status = status;
+        this.name = this.constructor.name;
+
+        Error.captureStackTrace(this, this.constructor);
+
+    }
+}
+
+module.exports= AppError;
